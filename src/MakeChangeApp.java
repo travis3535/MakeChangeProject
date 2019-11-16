fc93219aa48e3fad1c0cc678209be203c539518d
Travis Roberts <travis.c.roberts35@gmail.com>
2019-11-16 15:31:15 -0700

    added more if statements for correct price and not providing enough money. still need to refine the output.

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -7,15 +7,6 @@ public class MakeChangeApp {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 
-		double pennies = 0.01;
-		double nickles = 0.05;
-		double dimes = 0.10;
-		double quarters = 0.25;
-		int ones = 1;
-		int fives = 5;
-		int tens = 10;
-		int twenties = 20;
-
 		int penniesDue = 0;
 		int nicklesDue = 0;
 		int dimesDue = 0;
@@ -32,65 +23,61 @@ public class MakeChangeApp {
 		System.out.print("How much cash were you given?  $");
 		double tendered = keyboard.nextDouble();
 
-		System.out.println("Price " + price + " Tendered " + tendered);
+		System.out.println("Price $" + price + " Tendered $" + tendered);
 
 		double changeDue = (double) ((tendered - price) * 100) / 100;
 
-		System.out.println(changeDue + " changeDue");
+		System.out.println("Change Due: $" + changeDue);
+		
+		if(tendered < price) {
+			System.out.println("*** The customer did not provide enough money for purchase! ***");
+			System.exit(0);
+		}
+		if (tendered == price) {
+			System.out.println("Thank you for your purchase. No change due.");
+			System.exit(0);
+		}
+		
+		keyboard.close();
 
 		while (changeDue > 0) {
 			if (changeDue >= 20) {
-				// twentiesDue = (int) (twentiesDue + changeDue) / 20;
-				// changeDue -= twenties;
 				changeDue -= 20;
 				twentiesDue++;
 				continue;
 			}
 			if (changeDue >= 10) {
-//				tensDue = (int) (tensDue + changeDue) / 10;
-//				changeDue -= tens;
+				
 				changeDue -= 10;
 				tensDue++;
 				continue;
 			}
 			if (changeDue >= 5) {
-//				tensDue = (int) (tensDue + changeDue) / 10;
-//				changeDue -= tens;
 				changeDue -= 5;
 				fivesDue++;
 				continue;
 			}
 			if (changeDue >= 1) {
-//				tensDue = (int) (tensDue + changeDue) / 10;
-//				changeDue -= tens;
 				changeDue -= 1;
 				onesDue++;
 				continue;
 			}
 			if (changeDue >= .25) {
-//				tensDue = (int) (tensDue + changeDue) / 10;
-//				changeDue -= tens;
 				changeDue -= .25;
 				quartersDue++;
 				continue;
 			}
 			if (changeDue >= .10) {
-//				tensDue = (int) (tensDue + changeDue) / 10;
-//				changeDue -= tens;
 				changeDue -= .10;
 				dimesDue++;
 				continue;
 			}
 			if (changeDue >= .5) {
-//				tensDue = (int) (tensDue + changeDue) / 10;
-//				changeDue -= tens;
 				changeDue -= .5;
 				nicklesDue++;
 				continue;
 			}
 			if (changeDue >= .001) {
-//				tensDue = (int) (tensDue + changeDue) / 10;
-//				changeDue -= tens;
 				changeDue -= .01;
 				penniesDue++;
 				continue;
@@ -99,9 +86,10 @@ public class MakeChangeApp {
 			}
 
 		}
-		System.out.println(twentiesDue + " twenties " + tensDue + " tens " + fivesDue + " fives " + onesDue
-				+ " ones " + quartersDue + " quarters " + dimesDue + " dimes " + nicklesDue + " nickles "
-				+ penniesDue + " pennies " + changeDue + " remaining");
+		System.out.println("[" + twentiesDue + "]-twenties " + "[" + tensDue + "]-tens " + "[" 
+				+ fivesDue + "]-fives " + "[" + onesDue + "]-ones " + "[" + quartersDue + "]-quarters " 
+				+ "[" + dimesDue + "]-dimes " + "[" + nicklesDue + "]-nickles " + "[" +
+				+ penniesDue + "]-pennies ");
 
 	}
 
