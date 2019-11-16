a01b1df8fd8bd2688ba6a5457d636d7b5cee0612
Travis Roberts <travis.c.roberts35@gmail.com>
2019-11-16 16:50:01 -0700

    added conditions for not displaying denominations that were not being used. full working project.

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class MakeChangeApp {
-	static double price;
-	static double tendered;
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
@@ -15,21 +13,22 @@ public class MakeChangeApp {
 		int fivesDue = 0;
 		int tensDue = 0;
 		int twentiesDue = 0;
-
+		double price;
+		double tendered;
 
 		System.out.print("What was the price of the item?  $");
-		double price = keyboard.nextDouble();
+		price = keyboard.nextDouble();
 
 		System.out.print("How much cash were you given?  $");
-		double tendered = keyboard.nextDouble();
+		tendered = keyboard.nextDouble();
 
 		System.out.println("Price $" + price + " Tendered $" + tendered);
 
 		double changeDue = (double) ((tendered - price) * 100) / 100;
 
 		System.out.println("Change Due: $" + changeDue);
-		
-		if(tendered < price) {
+
+		if (tendered < price) {
 			System.out.println("*** The customer did not provide enough money for purchase! ***");
 			System.exit(0);
 		}
@@ -37,7 +36,7 @@ public class MakeChangeApp {
 			System.out.println("Thank you for your purchase. No change due.");
 			System.exit(0);
 		}
-		
+
 		keyboard.close();
 
 		while (changeDue > 0) {
@@ -47,7 +46,7 @@ public class MakeChangeApp {
 				continue;
 			}
 			if (changeDue >= 10) {
-				
+
 				changeDue -= 10;
 				tensDue++;
 				continue;
@@ -81,16 +80,38 @@ public class MakeChangeApp {
 				changeDue -= .01;
 				penniesDue++;
 				continue;
+			}
+			if (twentiesDue != 0) {
+				System.out.println("[" + twentiesDue + "]-twenties** ");
 			} else {
 				break;
 			}
 
 		}
-		System.out.println("[" + twentiesDue + "]-twenties " + "[" + tensDue + "]-tens " + "[" 
-				+ fivesDue + "]-fives " + "[" + onesDue + "]-ones " + "[" + quartersDue + "]-quarters " 
-				+ "[" + dimesDue + "]-dimes " + "[" + nicklesDue + "]-nickles " + "[" +
-				+ penniesDue + "]-pennies ");
+		if (twentiesDue != 0) {
+			System.out.print("[" + twentiesDue + "]-Twenties ");
+		}
+		if (tensDue != 0) {
+			System.out.print("[" + tensDue + "]-Tens ");
+		}
+		if (fivesDue != 0) {
+			System.out.print("[" + fivesDue + "]-Fives ");
+		}
+		if (onesDue != 0) {
+			System.out.print("[" + onesDue + "]-Ones ");
+		}
+		if (quartersDue != 0) {
+			System.out.print("[" + quartersDue + "]-Quarters ");
+		}
+		if (dimesDue != 0) {
+			System.out.print("[" + dimesDue + "]-Dimes ");
+		}
+		if (nicklesDue != 0) {
+			System.out.print("[" + nicklesDue + "]-Nickles ");
+		}
+		if (penniesDue != 0) {
+			System.out.print("[" + penniesDue + "]-Pennies ");
+		}
 
 	}
-
 }
